6d72a93aa804672ff0a27fa8500ab02179ab5bc3
jsfr <jfrdev@gmail.com>
2014-01-21 21:05:04 +0100

    all of it

--- servers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xterm -e "cd $HOME/repos/pcsd2013exam/acertainsupplychain/bin && java -classpath '.:../lib/*' com.acertainsupplychain.server.ItemSupplierHTTPServer 1 8083" &
-xterm -e "cd $HOME/repos/pcsd2013exam/acertainsupplychain/bin && java -classpath '.:../lib/*' com.acertainsupplychain.server.ItemSupplierHTTPServer 0 8082" &
-xterm -e "cd $HOME/repos/pcsd2013exam/acertainsupplychain/bin && java -classpath '.:../lib/*' com.acertainsupplychain.server.OrderManagerHTTPServer 0 8081" &
